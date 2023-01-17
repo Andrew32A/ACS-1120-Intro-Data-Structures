a1abe5566c4e1afafe616b9d3374a40cddbeac1c
Andrew Alsing <andrew32alsing@gmail.com>
2023-01-17 00:00:21 -0800

    added requested words arg

--- Code/dictionary_words.py
+++ Code/dictionary_words.py
@@ -1,11 +1,12 @@
 import random
+import sys
 
-def random_sentence_generator():
+def random_sentence_generator(words_requested = 5):
     dictionary = open("/usr/share/dict/words", "r") # opens and reads dictionary on unix devices
     words = dictionary.read().split()
     dictionary.close()
-    sentence = (random.sample(words, 5)) # randomly samples 5 words from dictionary
-    print(" ".join(sentence)) # removes brackets and commas by combining items in array into a string
+    sentence = (random.sample(words, words_requested)) # randomly samples words from dictionary, defaults to 5
+    print(" ".join(sentence)) # removes brackets and commas by combining items in array into a string then prints
 
 if __name__ == "__main__":
-    random_sentence_generator()
+    random_sentence_generator(int(sys.argv[1]))
