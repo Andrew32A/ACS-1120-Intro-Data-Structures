a8f7466dd2bdd90dfae5c96f13ef2322470e5c41
Andrew Alsing <andrew32alsing@gmail.com>
2023-01-22 16:21:36 -0800

    added filter for non-alphabetical characters in histogram

--- Code/histogram.py
+++ Code/histogram.py
@@ -4,20 +4,33 @@ source_text = "one fish two fish three fish four fish"
 histogram_storage = {}
 
 def histogram(source_text):
+    '''
+    splits source text into separate items then adds 1 to the key's value if true
+    '''
     split_text = source_text.split()
     for word in split_text:
-        if word in histogram_storage:
-            histogram_storage[word] += 1
+        word = word.lower()
+        if word[0].isalpha() == False: # removes non alphabetical strings by checking first index in string
+            pass
         else:
-            histogram_storage[word] = 1
+            if word in histogram_storage:
+                histogram_storage[word] += 1
+            else:
+                histogram_storage[word] = 1
 
     for word, number in histogram_storage.items():
         print(f"{word}: {number}")
 
 def unique_words(histogram):
+    '''
+    prints length of histogram
+    '''
     print(f"There are {len(histogram)} unique words in the source text")
 
 def frequency(word, histogram):
+    '''
+    prints value of key that was put in as an argument
+    '''
     try:
         print(f"The word {word} appears {histogram[word]} times in the source text")
     
@@ -27,5 +40,5 @@ def frequency(word, histogram):
 if __name__ == "__main__":
     histogram(source_text)
     unique_words(histogram_storage)
-    frequency(sys.argv[1], histogram_storage)
+    frequency(sys.argv[1].lower(), histogram_storage)
 
