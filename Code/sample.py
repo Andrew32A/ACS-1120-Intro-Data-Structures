520dd9cb7caf28ff0e575220565896f9d830849c
Andrew Alsing <andrew32alsing@gmail.com>
2023-01-24 21:22:25 -0800

    fixed probability output

--- Code/sample.py
+++ Code/sample.py
@@ -1,31 +1,36 @@
-import histogram
+from histogram import histogram
 import random
 
 def sampler(text_to_sample):
     '''
     returns one random word from histogram
     '''
-    random_word = random.sample(list(text_to_sample.items()), 1) # changed dict to a list to avoid deprecation warning "DeprecationWarning: Sampling from a set deprecated since Python 3.9 and will be removed in a subsequent version."
+    words_from_histogram = histogram(text_to_sample)
+    random_word = random.choices(
+        list(words_from_histogram.keys()), 
+        weights = words_from_histogram.values(), 
+        k = 1) [0] # changed dict to a list to avoid deprecation warning "DeprecationWarning: Sampling from a set deprecated since Python 3.9 and will be removed in a subsequent version."
 
     return random_word
 
 def probability(source_text):
     repeats = {}
-    words = []
-
-    for i in range(10000):
-        sampled_word = sampler(histogram.histogram(source_text))
-        sampled_word = sampled_word[0][0]
-        words.append(sampled_word)
-        repeats[words[i]] = words.count(words[i])
+    
+    for _ in range(10000):
+        sampled_word = sampler(source_text)
+        if sampled_word in repeats:
+            repeats[sampled_word] += 1
+        else:
+            repeats[sampled_word] = 1
 
     return repeats
 
 if __name__ == "__main__":
     source_text = "one fish two fish three fish four fish"
 
-    sample = sampler(histogram.histogram(source_text))
-    print(f"Random sample result: {sample[0][0]}: {sample[0][1]}")
+    sample = sampler(source_text)
+    print(sample)
 
     probability_output = probability(source_text)
-    print (probability_output)
+    for word in probability_output:
+        print(f"{word}: {probability_output[word]}")
