7ca091da3a9eb884dbcc2f8e08805a5648e01de3
Andrew Alsing <andrew32alsing@gmail.com>
2023-01-24 20:17:13 -0800

    added sampler function

--- Code/sample.py
+++ Code/sample.py
@@ -1,17 +1,19 @@
 import histogram
 import random
 
-source_text = "one fish two fish three fish four fish"
-
 def sampler(histogram):
     '''
     returns one random word from histogram
     '''
-    random_word = random.sample(histogram, 1)
+    random_word = random.sample(list(histogram.items()), 1) # changed dict to a list to avoid deprecation warning "DeprecationWarning: Sampling from a set deprecated since Python 3.9 and will be removed in a subsequent version."
 
+    return random_word
 
+def probability(histogram):
+    pass
 
 if __name__ == "__main__":
+    source_text = "one fish two fish three fish four fish"
     sample = sampler(histogram.histogram(source_text))
 
-    print(sample)
+    print(f"Random sample result: {sample[0][0]}: {sample[0][1]}")
