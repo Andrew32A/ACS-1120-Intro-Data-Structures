928d308a79bfb23875f1cc88ef2c69a980a93cc5
Andrew Alsing <andrew32alsing@gmail.com>
2023-02-14 21:38:01 -0800

    fixed find method and replaced lambda test with gradescope test

--- Code/linkedlist.py
+++ Code/linkedlist.py
@@ -95,7 +95,7 @@ class LinkedList:
         # TODO: Loop through all nodes to find item, if present return True otherwise False
         node = self.head
         while node is not None:
-            if matcher(node.data):
+            if matcher in node.data:
                 return True
             node = node.next
         return False
